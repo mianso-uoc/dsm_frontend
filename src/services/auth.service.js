107e079b0ac32ff41c5ee433b15c8e7df65edf98
mianso-uoc <Administrador@host.docker.internal>
2021-04-24 20:52:51 +0200

    Servicio para autenticación JWT

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = process.env.REACT_APP_BACKEND + "/api/auth/";
 
 class AuthService {
   login(email, password) {
